5785610f59536fd402bcd67c474315f0f087a068
Fernando Manso <brok3r@gmail.com>
2022-05-26 18:58:10 +0200

    Working on it

--- train.py
+++ train.py
@@ -1,5 +1,8 @@
 from unityagents import UnityEnvironment
 import numpy as np
+from dqn_agent import Agent
+import torch 
+from collections import deque
 
 env = UnityEnvironment(file_name="./Banana_Windows_x86_64/Banana.exe")
 
@@ -8,3 +11,51 @@ brain = env.brains[brain_name]
 
 print(brain_name)
 print(brain)
+
+agent = Agent(state_size=37, action_size=4, seed=0)
+
+def dqn(n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
+    """Deep Q-Learning.
+    
+    Params
+    ======
+        n_episodes (int): maximum number of training episodes
+        max_t (int): maximum number of timesteps per episode
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+    """
+    scores = []                        # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    eps = eps_start                    # initialize epsilon
+    for i_episode in range(1, n_episodes+1):        
+        info = env.reset(train_mode=True)[brain_name]
+        state = info.vector_observations[0]
+        score = 0
+        for t in range(max_t):
+            action = agent.act(state, eps)
+            # next_state, reward, done, _ = env.step(action)
+            info = env.step(action)[brain_name]
+            next_state = info.vector_observations[0]
+            reward = info.rewards[0]
+            done = info.local_done[0]
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+            if done:
+                break 
+        scores_window.append(score)       # save most recent score
+        scores.append(score)              # save most recent score
+        eps = max(eps_end, eps_decay*eps) # decrease epsilon
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+        if i_episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        if np.mean(scores_window)>=13.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
+            torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
+            break
+    return scores
+
+scores = dqn()
+#states = env.reset()
+#print(env.reset())
